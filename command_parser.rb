e4ffe8960b9b65a9d4ba8e7b15e8857e5b48e162
kdbanman <kdbanman@ualberta.ca>
2015-02-23 21:25:52 -0700

    implement background tasks

--- command_parser.rb
+++ command_parser.rb
@@ -11,7 +11,7 @@ class CommandParser < Contracted
 
   # recursively expands all environment variables (i.e. $VARs).
   # pre takes string
-  # post returns string, contains no matches for /$[^\s]+ /
+  # post returns string
   def substitute_vars(raw)
     raw
   end
@@ -20,7 +20,8 @@ class CommandParser < Contracted
   # pre takes string, must contain zero or one trailing " &"
   # post returns string, contains no trailing " &"
   def extract_background(raw)
-    is_background = false
+    is_background = !!(raw =~ /\s+&$/)
+    raw = raw.sub(/\s+&$/, '').strip
 
     [raw, is_background]
   end
@@ -99,29 +100,21 @@ class CommandParser < Contracted
         end
     )
 
-    noVariables = Contract.new(
-        "expanded string must not contain environment variables",
-        Proc.new do |result|
-          !(result =~ /\$[^\s]+/)
-        end
-    )
-
     noBackgrounder = Contract.new(
         "result string must have zero trailing ampersand",
         Proc.new do |result|
-          !(result =~ /\s+&$/)
+          !(result[0] =~ /\s+&$/)
         end
     )
 
     noRedirect = Contract.new(
         "result string must have zero file redirect operations",
         Proc.new do |result|
-          !(result =~ / >>?\s+[^\s]+/)
+          !(result[0] =~ / >>?\s+[^\s]+/)
         end
     )
 
     addPostcondition(:substitute_vars, returnString)
-    addPostcondition(:substitute_vars, noVariables)
 
     addPostcondition(:extract_background, returnStringBool)
     addPostcondition(:extract_background, noBackgrounder)
